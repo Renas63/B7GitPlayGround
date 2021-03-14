b41148a76e88baa09ee9ada50415b19b2d665de0
Renas63 <75856707+Renas63@users.noreply.github.com>
2021-03-14 13:05:57 -0500

    third code

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("firts code");
+        System.out.println("first code");
 
         System.out.println("second code");
+        System.out.println("third code");
     }
 }
